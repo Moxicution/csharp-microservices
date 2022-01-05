bd3a4d13e17f169a4d05e6b9fd4d866197e456bd
Carlos Schipper <carlos.schipper@soundofdata.nl>
2022-01-05 13:37:54 +0100

    Add Repository for platform

---
+++ PlatformService/Data/IPlatformRepository.cs
@@ -0,0 +1,12 @@
+namespace PlatformService.Data
+{
+    using PlatformService.Models;
+
+    public interface IPlatformRepository
+    {
+        bool SaveChanges();
+        IEnumerable<Platform> GetAllPlatforms();
+        Platform GetPlatformById(int id);
+        void CreatePlatform(Platform platform);
+    }
+}
