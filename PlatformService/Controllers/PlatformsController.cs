ae29ae8f82273e0a3cf4b3198435ecae07185f6e
Carlos Schipper <carlos.schipper@soundofdata.nl>
2022-01-05 16:53:29 +0100

    Added controllers

---
+++ PlatformService/Controllers/PlatformsController.cs
@@ -0,0 +1,55 @@
+namespace PlatformService.Controllers
+{
+    using AutoMapper;
+    using Microsoft.AspNetCore.Mvc;
+    using PlatformService.Data;
+    using PlatformService.Dtos;
+    using PlatformService.Models;
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PlatformsController : ControllerBase
+    {
+        private readonly IPlatformRepository _repo;
+        private readonly IMapper _mapper;
+        public PlatformsController(IPlatformRepository repo, IMapper mapper)
+        {
+            _repo = repo;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<PlatformReadDto>> GetPlatforms()
+        {
+            Console.WriteLine("--> Getting platforms...");
+
+            var platformItems = _repo.GetAllPlatforms();
+            return Ok(_mapper.Map<IEnumerable<PlatformReadDto>>(platformItems));
+        }
+
+        [HttpGet("{id}", Name = "GetPlatformById")]
+        public ActionResult<PlatformReadDto> GetPlatformById(int id)
+        {
+            Console.WriteLine("--> Getting platform...");
+
+            var platformItem = _repo.GetPlatformById(id);
+            if (platformItem == null)
+                return NotFound();
+
+            return Ok(_mapper.Map<PlatformReadDto>(platformItem));
+        }
+
+        [HttpPost]
+        public ActionResult<PlatformReadDto> CreatePlatform(PlatformCreateDto platform)
+        {
+            Console.WriteLine("--> Creating platform...");
+            
+            var platformModel = _mapper.Map<Platform>(platform);
+            _repo.CreatePlatform(platformModel);
+            _repo.SaveChanges();
+
+            var platformReadDto = _mapper.Map<PlatformReadDto>(platformModel);
+            return CreatedAtRoute(nameof(GetPlatformById), new { Id = platformReadDto.Id }, platformReadDto);
+        }
+    }
+}
--- PlatformService/Program.cs
+++ PlatformService/Program.cs
@@ -6,26 +6,21 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
 builder.Services.AddDbContext<AppDbContext>(opt =>opt.UseInMemoryDatabase("AppDbInMem"));
 builder.Services.AddScoped<IPlatformRepository, PlatformRepository>();
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
 var app = builder.Build();
 
-PrepDb.PrepPopulation(app);
-
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+    PrepDb.PrepPopulation(app);
 }
 
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
 
 app.Run();
